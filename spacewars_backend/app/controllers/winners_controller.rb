54806636a2a3be5a2bc0f9d57cc8f6727bf2b418
joshpled <joshuapleduc@gmail.com>
2020-11-01 22:46:33 -0500

    add serializer to winner

--- spacewars_backend/app/controllers/winners_controller.rb
+++ spacewars_backend/app/controllers/winners_controller.rb
@@ -2,6 +2,7 @@ class WinnersController < ApplicationController
     
     def index
         winners = Winner.all
+        render json: WinnerSerializer.new(winners).serializable_hash
     end
 
 end
--- spacewars_backend/app/serializers/winner_serializer.rb
+++ spacewars_backend/app/serializers/winner_serializer.rb
@@ -1,4 +1,18 @@
 class WinnerSerializer
   include FastJsonapi::ObjectSerializer
   attributes :initials, :score
+
+  def serializable_hash
+    data = super
+    if data[:data].is_a? Hash
+      return data[:data]
+    elsif data[:data].is_a? Array
+      data[:data].map { |x| x[:attributes] }
+    elsif data[:data] == nil
+      nil
+    else
+      data
+    end
+  end
+
 end
